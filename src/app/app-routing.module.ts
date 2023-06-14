f5a380155c12a8e4fa11a960bdaa43201cb16b47
Wisthong Martinez <wisthongd@gmail.com>
2023-06-14 17:01:22 -0500

    Video 3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch:'full'
+  },
+  {
+    path: 'login',
+    loadComponent() {
+      return import('./pages/login/login.component');
+    },
+    title: 'Login',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
